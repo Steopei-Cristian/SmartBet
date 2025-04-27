d141db5e25ad2f7ef92b1a1da44e4f2c0a8d8949
Steopei-Cristian <crististio2004@gmail.com>
2025-04-27 16:58:01 +0300

    backend first commit

---
+++ backend/src/main/java/smart/bet/SmartBetApp.java
@@ -0,0 +1,11 @@
+package smart.bet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBetApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBetApp.class, args);
+    }
+}
